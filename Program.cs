b68096b540170d14fa145079240259de53936088
lex-avoidant <kylexyanjasherenemmicaller@iskolarngbayan.pup.edu.ph>
2026-04-15 03:42:02 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
+using System;
 
+class Product;
+[
+  public int Id;
+  public string Name;
+  public double Price;
+  public int RemainingStock;
+
+  public void DisplayProduct()
+  {
+    Console.WriteLine($"{Id}, {Name}, -₱{Price} (Stock: {RemainingStock})");
+  }
+
+  public double ComputeTotal(int quantity)
+  {
+    return Price * quantity;
+  }
+
+  public bool IsAvailable(int quantity)
+  {
+    return quantity <= RemainingStock;
+  }
+
+  public void ReduceStock(int quantity)
+  {
+    RemainingStock -= quantity;
+  }
